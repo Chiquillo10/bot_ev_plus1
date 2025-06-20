a795de8639f86b3377cdeddfa04328917ce3571d
Chiquillo10 <esmalochichi10@gmail.com>
2025-06-20 00:44:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import requests
+import os
 
-BOT_TOKEN = "7738392758:AAFsMfvVcA1xingWTYmwdBjkR2DO6ozP8Ek"
-CHAT_ID = "7943883784"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
 def enviar_mensaje(texto):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     data = {"chat_id": CHAT_ID, "text": texto}
     requests.post(url, data=data)
 
-mensaje = "📊 Nuevo Pick EV+
+mensaje = (
+    "📊 Nuevo Pick EV+\n\n"
+    "🏟️ Partido: Team A vs Team B\n"
+    "✅ Probabilidad: 72%\n"
+    "💸 Cuota: 1.80\n"
+    "📈 EV+: 0.15"
+)
 
-🏟️ Partido: Team A vs Team B
-✅ Probabilidad: 72%\n💸 Cuota: 1.80\n📈 EV+: 0.15"
 enviar_mensaje(mensaje)
